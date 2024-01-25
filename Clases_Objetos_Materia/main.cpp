65bdd2013e76699bc83df2386d78964382d90642
BrandonFilth <112527641+BrandonFilth@users.noreply.github.com>
2024-01-25 17:57:38 -0600

    Update main.cpp

--- Clases_Objetos_Materia/main.cpp
+++ Clases_Objetos_Materia/main.cpp
@@ -34,8 +34,10 @@ public:
 void menu(Materia &materia){
     int opcion, clave;
     string nombre, profesor, libro;
+
+    do{
     system("CLS");
-    cout<<"1)Cambiar clave\n2)Cambiar profesor\n3)Imprimir detalles\nSelecciona una opcion:"<<endl;
+    cout<<"1)Cambiar clave\n2)Cambiar profesor\n3)Imprimir detalles\n4)salir\nSelecciona una opcion:"<<endl;
     cin>>opcion;
 
     switch (opcion){
@@ -51,20 +53,23 @@ void menu(Materia &materia){
         materia.cambiaProfe(profesor);
     case 3:
         materia.imprime();
+        system("pause");
         break;
-    
+
     default:
         cout<<"Opcion invalida"<<endl;
+        system("pause");
         break;
     }
-
+    }while(opcion !=4);
 }
 
 int main(){
     int opcion;
     Materia programacion(8812,"Programacion", "Jose Cardenas", "Fundamentos de programacion");
     Materia basesDatos(8815, "Bases de Datos", "Manuel Perez", "Las bases de datos");
-
+    do{
+            system("CLS");
     cout<<"Materias disponibles:\n1)Programacion\n2)Bases de datos\nSeleccione una materia a modificar:"<<endl;
     cin>>opcion;
 
@@ -76,8 +81,9 @@ int main(){
     }
     else{
         cout<<"Opcion invalida"<<endl;
+        system("pause");
     }
-
+    }while(opcion != 3);
     return 0;
 
 }
